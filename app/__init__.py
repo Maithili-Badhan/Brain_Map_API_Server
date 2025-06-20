c3ea9731ac2cacae7405979d2b606c388f8df960
Maithili-Badhan <maithilibprojects@gmail.com>
2025-06-21 03:51:55 +0530

    Last Final Commit

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
+from flask_cors import CORS
 
 load_dotenv()
 
@@ -15,6 +16,8 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+
+    CORS(app)  # Enable CORS for the app
 
     # Import and register Blueprints here to avoid circular import
     from app.routes.regions import regions_bp
